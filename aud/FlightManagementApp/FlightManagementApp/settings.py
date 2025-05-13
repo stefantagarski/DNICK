fb56a4e3d4306d557086121f65c1234904bd2963
stefantagarski <stefantagarski@gmail.com>
2025-05-13 23:44:24 +0200

    django forms and views

--- aud/FlightManagementApp/FlightManagementApp/settings.py
+++ aud/FlightManagementApp/FlightManagementApp/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+from django.conf.global_settings import STATICFILES_DIRS
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -119,6 +121,10 @@ STATIC_URL = 'static/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- aud/FlightManagementApp/flightapp/views.py
+++ aud/FlightManagementApp/flightapp/views.py
@@ -1,9 +1,9 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
+
+from .forms import ContactForm, FlightForm
+from .models import Flight
 from datetime import datetime
-
-from django.shortcuts import render
-
-from flightapp.models import Flight
-
 
 # Create your views here.
 def index(request):
@@ -15,3 +15,41 @@ def details(request, flight_id):
     flight = Flight.objects.filter(id = flight_id).first()
     context = {'flight_data': flight, 'app_name': 'flightapp'}
     return render(request, 'details.html', context)
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+            sender = form.cleaned_data['sender']
+            cc_myself = form.cleaned_data['cc_myself']
+            # Process the data as needed
+            return redirect('index')
+
+    form = ContactForm()
+    return  render(request, "contact.html", {'form': form})
+
+def add_flight(request):
+    if request.method == 'POST':
+        form = FlightForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+        return redirect('index')
+
+    form = FlightForm()
+    return render(request, "add_flight.html", context={'form': form})
+
+def edit_flight(request, flight_id):
+    flight = get_object_or_404(Flight, pk=flight_id)
+    # Flight.objects.filter(pk=flight_id).exists()
+
+    if request.method == 'POST':
+        form = FlightForm(request.POST, request.FILES, instance=flight)
+        if form.is_valid():
+            form.save()
+
+        return redirect('index')
+
+    form = FlightForm(instance=flight)
+    return render(request, "edit_flight.html", context={'form': form, 'flight_id': flight_id})
